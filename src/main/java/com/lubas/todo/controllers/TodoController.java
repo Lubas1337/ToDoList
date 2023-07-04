62199b592c7f1bfcec266def83a1f1f6ed3714fe
lubas1337 <abylailuba@gmail.com>
2023-07-05 01:20:51 +0600

    Дополнил

--- src/main/java/com/lubas/todo/controllers/TodoController.java
+++ src/main/java/com/lubas/todo/controllers/TodoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/todos")
+@RequestMapping("/")
     public class TodoController {
         private final TodoService todoService;
 
